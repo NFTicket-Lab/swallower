c62701f16cbe1db979054c2411da4730685f6a33
kaiweicai <kaiweicai@163.com>
2021-12-31 19:28:31 +0800

    add minSwallower

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import {ApiPromise,WsProvider} from '@polkadot/api';
 import {Keyring} from '@polkadot/keyring';
+import { cryptoWaitReady, mnemonicGenerate } from '@polkadot/util-crypto';
 
+// we only need to do this once per app, somewhere in our init code
+// (when using the API and waiting on `isReady` this is done automatically)
+await cryptoWaitReady();
 
 const connectSubstrate = async () => {
     const wsProvider = new WsProvider('ws://127.0.0.1:9944');
@@ -8,10 +12,33 @@ const connectSubstrate = async () => {
     return api;
   };
 
-  const keyring = new Keyring({type:'sr25519'});
-const bobpair = ()=>{
-  return keyring.createFromUri('//Bob');
+  // const keyring = new Keyring({type:'sr25519'});
+const getAccount = (account)=>{
+  return accountMap.get(account);
+  // console.log("accountMap is:",accountMap);
 };
+
+var accountMap = new Map();
+const keyring = new Keyring({ type: 'sr25519' });
+
+const initAccount = ()=>{
+  const ALICE = keyring.addFromUri('//Alice');
+  const BOB = keyring.addFromUri('//Bob');
+  const CHARLIE = keyring.addFromUri('//Charlie');
+  const DAVE = keyring.addFromUri('//Dave');
+  const EVE = keyring.addFromUri('//Eve');
+  const FERDIE = keyring.addFromUri('//Ferdie');
+  accountMap.set('ALICE',ALICE);
+  accountMap.set('BOB',BOB);
+  accountMap.set('CHARLIE',CHARLIE);
+  accountMap.set('DAVE',DAVE);
+  accountMap.set('EVE',EVE);
+  accountMap.set('FERDIE',FERDIE);
+}
+
+initAccount();
+
+
  const maindata = async () => {
     const api = await connectSubstrate();
     // 取得链上 meta-data. 去掉下面 comment 去看链上 meta-data. 是一个挺大的 JSON 文件
@@ -21,7 +48,7 @@ const bobpair = ()=>{
     return condata;
   };
 
-  export default connectSubstrate;
+  export {connectSubstrate,getAccount};
 
 //   main()
 //   .then(() => {
--- api/server.js
+++ api/server.js
@@ -1,130 +1,136 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectSubstrate from './index.js'
+import { connectSubstrate, getAccount } from './index.js'
 import { cryptoWaitReady, mnemonicGenerate } from '@polkadot/util-crypto';
-import {Keyring} from '@polkadot/keyring';
+import { Keyring } from '@polkadot/keyring';
 // import bobpair from './pair.js';
 
 dotenv.config({
-    path:'./config/config.env',
+    path: './config/config.env',
 })
 
 const app = express()
 
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
 const api = await connectSubstrate();
 
-app.get("/api",async (req,res)=>{
+
+
+const BOB_ADDRESS = "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty";
+
+
+app.get("/api/metaData", async (req, res) => {
     // const api = await connectSubstrate();
-    const [magicNumber,metadata] = await api.rpc.state.getMetadata();
+    const [magicNumber, metadata] = await api.rpc.state.getMetadata();
     // const addr='5DTestUPts3kjeXSTMyerHihn1uwMfLj8vU8sqF7qYrFabHE';
     // const acct = await api.query.system.account(addr)
-    res.status(200).json({success:true,msg:metadata})
+    res.status(200).json({ success: true, msg: metadata })
 })
 
-app.get("/swallower/admin",async (req,res)=>{
+app.get("/swallower/admin", async (req, res) => {
     const admin = await api.query.swallower.admin();
-    res.status(200).json({admin:admin})
+    res.status(200).json({ admin: admin })
 });
 
-app.get("/swallower/assetAmount",async (req,res)=>{
+app.get("/swallower/assetAmount", async (req, res) => {
     const assetAmount = await api.query.swallower.assetAmount();
-    res.status(200).json({assetAmount:assetAmount})
+    res.status(200).json({ assetAmount: assetAmount })
 });
 
-app.get("/swallower/assetId",async (req,res)=>{
+app.get("/swallower/assetId", async (req, res) => {
     const assetId = await api.query.swallower.assetId();
-    res.status(200).json({assetId:assetId})
+    res.status(200).json({ assetId: assetId })
 });
 
-app.get("/swallower/battleZoneRewardMap",async (req,res)=>{
+app.get("/swallower/battleZoneRewardMap", async (req, res) => {
     const battleZoneRewardMap = await api.query.swallower.battleZoneRewardMap(req.query.hash);
-    res.status(200).json({battleZoneRewardMap:battleZoneRewardMap})
+    res.status(200).json({ battleZoneRewardMap: battleZoneRewardMap })
 });
 
-app.get("/swallower/geneAmount",async (req,res)=>{
+app.get("/swallower/geneAmount", async (req, res) => {
     const geneAmount = await api.query.swallower.geneAmount();
-    res.status(200).json({geneAmount:geneAmount})
+    res.status(200).json({ geneAmount: geneAmount })
 });
 
-app.get("/swallower/manager",async (req,res)=>{
+app.get("/swallower/manager", async (req, res) => {
     const manager = await api.query.swallower.manager();
-    res.status(200).json({manager:manager})
+    res.status(200).json({ manager: manager })
 });
 
-app.get("/swallower/ownerSwallower",async (req,res)=>{
+app.get("/swallower/ownerSwallower", async (req, res) => {
     const ownerSwallower = await api.query.swallower.ownerSwallower(req.query.accountId);
-    res.status(200).json({ownerSwallower:ownerSwallower})
+    res.status(200).json({ ownerSwallower: ownerSwallower })
 });
 
-app.get("/swallower/safeZone",async (req,res)=>{
+app.get("/swallower/safeZone", async (req, res) => {
     const safeZone = await api.query.swallower.safeZone(req.query.hash);
-    res.status(200).json({safeZone:safeZone})
+    res.status(200).json({ safeZone: safeZone })
 });
 
-app.get("/swallower/swallowerAmount",async (req,res)=>{
+app.get("/swallower/swallowerAmount", async (req, res) => {
     const swallowerAmount = await api.query.swallower.swallowerAmount();
-    res.status(200).json({swallowerAmount:swallowerAmount})
+    res.status(200).json({ swallowerAmount: swallowerAmount })
 });
 
-app.get("/swallower/swallowerNo",async (req,res)=>{
+app.get("/swallower/swallowerNo", async (req, res) => {
     const swallowerNo = await api.query.swallower.swallowerNo();
-    res.status(200).json({swallowerNo:swallowerNo})
+    res.status(200).json({ swallowerNo: swallowerNo })
 });
 
 
-app.get("/swallower/swallowers",async (req,res)=>{
+app.get("/swallower/swallowers", async (req, res) => {
     const swallowers = await api.query.swallower.swallowers(req.query.hash);
-    res.status(200).json({swallowers:swallowers})
+    res.status(200).json({ swallowers: swallowers })
 });
 
-app.get("/swallower/mintSwallower",async (req,res)=>{
-    //api.rx.swallower.mintSwallower()
-    const value = 3000n * 1000000n;
-    const gasLimit = 3000n * 1000000n;//不限制gas
-    await cryptoWaitReady(); 
-    const BOB_ADDRESS = "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty";
-    const keyring = new Keyring({ type: 'sr25519' });
-    const BOB = keyring.addFromUri('//Bob');
+const handleEvents = ({ events = [], status }) =>{
+    console.log('Transaction status:', status.type);
+
+    if (status.isInBlock) {
+        console.log('Included at block hash', status.asInBlock.toHex());
+        console.log('Events:');
+
+        events.forEach(({ event: { data, method, section }, phase }) => {
+            let event_msg =  '\t'+ phase.toString()+ `: ${section}.${method}`+ data.toString();
+            console.log(event_msg);
+            result.push({ data, method, section });
+        });
+    } else if (status.isFinalized) {
+        result.push(status.asFinalized.toHex());
+        console.log('Finalized block hash', status.asFinalized.toHex());
+        res.status(200).json({ mintSwallower: result });
+    }
+}
+
+app.get("/swallower/mintSwallower", async (req, res) => {
+    await cryptoWaitReady();
+
+    var account = getAccount(req.query.account);
+    console.log("req.query.account is:", account);
     // Get the nonce for the admin key
-    const { nonce } = await api.query.system.account(BOB_ADDRESS);
-    const mintSwallower = api.tx.swallower.mintSwallower(req.query.name);
-    let singer = await mintSwallower.signAsync(BOB);
-    console.log('singer data:',singer.toJSON());
-    singer.send(({ events = [], status }) => {
+    const { nonce } = await api.query.system.account(account.address);
+    const mintSwallower = await api.tx.swallower.mintSwallower(req.query.name);
+    let result = new Array();
+    await mintSwallower.signAndSend(account, ({ events = [], status }) => {
         console.log('Transaction status:', status.type);
-  
-        if (status.isInBlock) {
-          console.log('Included at block hash', status.asInBlock.toHex());
-          console.log('Events:');
-  
-          events.forEach(({ event: { data, method, section }, phase }) => {
-            console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
-          });
-        } else if (status.isFinalized) {
-          console.log('Finalized block hash', status.asFinalized.toHex());
-  
-        //   process.exit(0);
-        }
-      });
-    // mintSwallower.signAndSend(BOB, { nonce }, ({ events = [], status }) => {
-    //     console.log('Transaction status:', status.type);
-  
-    //     if (status.isInBlock) {
-    //       console.log('Included at block hash', status.asInBlock.toHex());
-    //       console.log('Events:');
-  
-    //       events.forEach(({ event: { data, method, section }, phase }) => {
-    //         console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
-    //       });
-    //     } else if (status.isFinalized) {
-    //       console.log('Finalized block hash', status.asFinalized.toHex());
-  
-    //     //   process.exit(0);
-    //     }
-    //   });
 
-    res.status(200).json({mintSwallower:"SUCCESS"})
+        if (status.isInBlock) {
+            console.log('Included at block hash', status.asInBlock.toHex());
+            console.log('Events:');
+
+            events.forEach(({ event: { data, method, section }, phase }) => {
+                let event_msg =  '\t'+ phase.toString()+ `: ${section}.${method}`+ data.toString();
+                console.log(event_msg);
+                result.push({ data, method, section });
+            });
+        } else if (status.isFinalized) {
+            result.push(status.asFinalized.toHex());
+            console.log('Finalized block hash', status.asFinalized.toHex());
+            res.status(200).json({ mintSwallower: result });
+        }
+        
+    });
+
 });
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
